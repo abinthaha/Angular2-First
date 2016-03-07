dcdf17620d7f16f5f59dd91be2cfc42fb195775d
Abin Thaha Azeez <abin@qburst.com>
2016-03-07 15:40:16 +0530

    Some changes

--- app/app.component.js
+++ app/app.component.js
@@ -1,7 +1,7 @@
 (function(app) {
     app.AppComponent = ng.core.Component({
         selector: 'my-app',
-        template: '<h1>My First Angular 2 App</h1>'
+        template: '<h1>Hello World, Its my First Angular 2 App</h1>'
     })
     .Class({
         constructor: function() {}
